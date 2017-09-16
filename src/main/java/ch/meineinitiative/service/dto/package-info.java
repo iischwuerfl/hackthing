dc771781ce20b76fd43de6e2415d533dee29451c
iischwuerfl <jennifer.studer@hotmail.com>
2017-09-16 07:52:10 +0200

    initial jhipster commit

---
+++ src/main/java/ch/meineinitiative/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package ch.meineinitiative.service.dto;
---
+++ src/main/webapp/app/app.module.ts
@@ -0,0 +1,55 @@
+import './vendor.ts';
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { Ng2Webstorage } from 'ng2-webstorage';
+
+import { MeineInitiativeSharedModule, UserRouteAccessService } from './shared';
+import { MeineInitiativeHomeModule } from './home/home.module';
+import { MeineInitiativeAdminModule } from './admin/admin.module';
+import { MeineInitiativeAccountModule } from './account/account.module';
+import { MeineInitiativeEntityModule } from './entities/entity.module';
+
+import { customHttpProvider } from './blocks/interceptor/http.provider';
+import { PaginationConfig } from './blocks/config/uib-pagination.config';
+
+// jhipster-needle-angular-add-module-import JHipster will add new module here
+
+import {
+    JhiMainComponent,
+    LayoutRoutingModule,
+    NavbarComponent,
+    FooterComponent,
+    ProfileService,
+    PageRibbonComponent,
+    ErrorComponent
+} from './layouts';
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        LayoutRoutingModule,
+        Ng2Webstorage.forRoot({ prefix: 'jhi', separator: '-'}),
+        MeineInitiativeSharedModule,
+        MeineInitiativeHomeModule,
+        MeineInitiativeAdminModule,
+        MeineInitiativeAccountModule,
+        MeineInitiativeEntityModule,
+        // jhipster-needle-angular-add-module JHipster will add new module here
+    ],
+    declarations: [
+        JhiMainComponent,
+        NavbarComponent,
+        ErrorComponent,
+        PageRibbonComponent,
+        FooterComponent
+    ],
+    providers: [
+        ProfileService,
+        customHttpProvider(),
+        PaginationConfig,
+        UserRouteAccessService
+    ],
+    bootstrap: [ JhiMainComponent ]
+})
+export class MeineInitiativeAppModule {}
